ee60c283db78ab5c8a2383219da6d17fae5e641e
airwingyangyu <yangyu@airwing.ai>
2019-04-28 14:36:16 +0800

    updata chinese

--- command-not-found.py
+++ command-not-found.py
@@ -59,3 +59,72 @@ messages=[
         "How many times do I have to flush before you go away?",
 ]
 print("\033[1;31;40m%s！\033[0m"%messages[count])
+count2 = random.randint(0, 33)
+if count2 == 0:
+    print("\033[1;%d;40m 没事偷着乐是不太可能了，偷着胖还是有点儿希望。\033[0m" %random.randint(31, 36))
+if count2 == 1:
+    print ("\033[1;32;%dm 你好菜哦! \033[0m"%random.randint(41, 46))
+if count2 == 2:
+    print ("\033[1;32;%dm 永远有多远？你小子就给我滚多远！\033[0m"%random.randint(41, 46))
+if count2 == 3:
+    print ("\033[1;32;%dm 虚伪的真诚，比魔鬼还可怕。\033[0m"%random.randint(41, 46))
+if count2 == 4:
+    print ("\033[1;32;%dm 有一种人，就爱搬起石头砸自己的脚。\033[0m"%random.randint(41, 46))
+if count2 == 5:
+    print ("\033[1;32;%dm 您长的背章！\033[0m"%random.randint(41, 46))
+if count2 == 6:
+    print ("\033[1;32;%dm 苯苯苯苯哦！\033[0m"%random.randint(41, 46))
+if count2 == 7:
+    print ("\033[1;32;%dm 君子成人之美，不成人之恶。小人反是。\033[0m"%random.randint(41, 46))
+if count2 == 8:
+    print ("\033[1;32;%dm 人人都说你丑，其实你只是美得不明显。\033[0m"%random.randint(41, 46))
+if count2 == 9:
+    print ("\033[1;32;%dm 真不好意思，让您贱笑了。\033[0m"%random.randint(41, 46))
+if count2 == 10:
+    print ("\033[1;%d;44m 我又不是人民币，怎么能让人人都喜欢我？！\033[0m"%random.randint(31, 36))
+if count2 == 11:
+    print ("\033[1;32;%dm 幸福是什么？幸福就是你吃鱼，我吃肉，看着别人啃骨头。\033[0m"%random.randint(41, 46))
+if count2 == 12:
+    print ("\033[1;32;%dm 您的五官组织纪律性太差！\033[0m"%random.randint(41, 46))
+if count2 == 13:
+    print ("\033[1;32;%dm 君子易事而难说也。\033[0m"%random.randint(41, 46))
+if count2 == 14:
+    print ("\033[1;32;%dm 长得丑的我见多了，但长成你这样就太过分了啊！\033[0m"%random.randint(41, 46))
+if count2 == 15:
+    print ("\033[1;%d;43m 怎么说呢，只要你的贱不影响到我们就好。\033[0m"%random.randint(31, 36))
+if count2 == 16:
+    print ("\033[1;32;%dm 猛的一看你不怎么样，仔细一看还不如猛的一看。\033[0m"%random.randint(41, 46))
+if count2 == 17:
+    print ("\033[1;32;%dm 别人要开飞机去撞双子星才行，而你只要跳伞就有同样的威力。\033[0m"%random.randint(41, 46))
+if count2 == 18:
+    print ("\033[1;32;%dm 你长得吧，怎么说呢，像素比较低。\033[0m"%random.randint(41, 46))
+if count2 == 19:
+    print ("\033[1;32;%dm 你哪里是饭桶，你简直就是米缸。\033[0m"%random.randint(41, 46))
+if count2 == 20:
+    print ("\033[1;32;%dm 长的很无辜，长的对没有起人仄易远对没有起党。\033[0m"%random.randint(41, 46))
+if count2 == 21:
+    print ("\033[1;32;%dm 别跟别人说你认识我，那就是侮辱！\033[0m"%random.randint(41, 46))
+if count2 == 22:
+    print ("\033[1;32;%dm 给你点阳光你就灿烂，给你点洪水你就泛滥，给你点颜色你就开染房。\033[0m"%random.randint(41, 46))
+if count2 == 23:
+    print ("\033[1;32;%dm 你是孙悟空的师弟，沙悟净的师兄。\033[0m"%random.randint(41, 46))
+if count2 == 24:
+    print ("\033[1;32;%dm 动物穿这衣服都变成人了，你丫一穿上立马变动物。\033[0m"%random.randint(41, 46))
+if count2 == 25:
+    print ("\033[1;32;%dm 真想立马把我37码的鞋bia到你42码的脸上。\033[0m"%random.randint(41, 46))
+if count2 == 26:
+    print ("\033[1;32;%dm 鲜花往往不属于赏花的人，而属于牛粪。\033[0m"%random.randint(41, 46))
+if count2 == 27:
+    print ("\033[1;32;%dm 长的很科幻，长的很笼统！\033[0m"%random.randint(41, 46))
+if count2 == 28:
+    print ("\033[1;32;%dm 想你想的饭都吃不下，太恶心了！\033[0m"%random.randint(41, 46))
+if count2 == 29:
+    print ("\033[1;32;%dm 你长的挺有创意活得挺有勇气。\033[0m"%random.randint(41, 46))
+if count2 == 30:
+    print ("\033[1;32;%dm 你脑子进水没安排水管吧？\033[0m"%random.randint(41, 46))
+if count2 == 31:
+    print ("\033[1;32;%dm 你去剪头吧，换个发型，就你这样的把脸档上是最好看的了。\033[0m"%random.randint(41, 46))
+if count2 == 32:
+    print ("\033[1;32;%dm 你需要回炉重造。\033[0m"%random.randint(41, 46))
+
+
